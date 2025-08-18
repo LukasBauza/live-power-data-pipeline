f4040c382b4c8fa70ec3e5ffa579f751e6a46f2b
Lukas Bauza <lukasbauza.lb@proton.me>
2025-08-18 17:00:24 +0100

    load data for day received

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from entsoe.entsoe import EntsoePandasClient
+import pandas
 
+# Connect to the client using the API key.
+# TODO: Check if this is the best way to use the api key.
+client = EntsoePandasClient(api_key="944ec6ae-94a0-4508-9198-d85bd9d09cdc")
+
+start = pandas.Timestamp("2025-08-17", tz="Europe/Dublin")
+end = pandas.Timestamp("2025-08-18", tz="Europe/Dublin")
+country_code = "IE"
+
+out = client.query_load(country_code, start=start, end=end)
+
+print(out)
