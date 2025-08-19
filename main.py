4f1a3ed5a0cd8cad29785d1602e054365c4d4de6
Lukas Bauza <lukasbauza.lb@proton.me>
2025-08-19 13:50:37 +0100

    Basic fetch of load electricity data

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from entsoe.entsoe import EntsoePandasClient
 import pandas
+import time
 
 # Connect to the client using the API key.
-# TODO: Check if this is the best way to use the api key.
 client = EntsoePandasClient(api_key="944ec6ae-94a0-4508-9198-d85bd9d09cdc")
 
-start = pandas.Timestamp("2025-08-17", tz="Europe/Dublin")
-end = pandas.Timestamp("2025-08-18", tz="Europe/Dublin")
+# NOTE: Optional parameters available only for Outages.
+start = pandas.Timestamp("2026-08-17", tz="Europe/Dublin")
+end = pandas.Timestamp("2026-08-18", tz="Europe/Dublin")
 country_code = "IE"
 
-out = client.query_load(country_code, start=start, end=end)
 
-print(out)
+def main():
+    while True:
+        now = pandas.Timestamp.now(tz="Europe/Dublin").floor("h")
+        start = now - pandas.Timedelta("1h")
+        end = now
+
+        df = client.query_load("IE", start=start, end=end)
+        print(f"Data at {now}:\n", df)
+
+        time.sleep(3600)
+
+
+if __name__ == "__main__":
+    main()
