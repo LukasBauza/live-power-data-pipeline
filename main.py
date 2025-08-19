c2d99d6e71f57530679fb50a98a0aa10c705c2da
Lukas Bauza <lukasbauza.lb@proton.me>
2025-08-19 13:55:52 +0100

    updated main.py to use config.py information

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ from entsoe.entsoe import EntsoePandasClient
 import pandas
 import time
 
-# Connect to the client using the API key.
-client = EntsoePandasClient(api_key="944ec6ae-94a0-4508-9198-d85bd9d09cdc")
+from src import config
 
-# NOTE: Optional parameters available only for Outages.
-start = pandas.Timestamp("2026-08-17", tz="Europe/Dublin")
-end = pandas.Timestamp("2026-08-18", tz="Europe/Dublin")
-country_code = "IE"
+# Connect to the client using the API key.
+client = EntsoePandasClient(api_key=config.API_KEY)
 
 
 def main():
     while True:
-        now = pandas.Timestamp.now(tz="Europe/Dublin").floor("h")
+        now = pandas.Timestamp.now(tz=config.TIME_ZONE).floor("h")
         start = now - pandas.Timedelta("1h")
         end = now
 
-        df = client.query_load("IE", start=start, end=end)
+        df = client.query_load(config.COUNTRY_CODE, start=start, end=end)
         print(f"Data at {now}:\n", df)
 
         time.sleep(3600)
