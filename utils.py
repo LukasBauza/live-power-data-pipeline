f327899d2d6d3f7a28562c7ca9cb9755824ac486
Lukas Bauza <lukasbauza.lb@proton.me>
2025-08-19 13:51:59 +0100

    removed utls.py, making code more modular

--- utils.py
+++
@@ -1,7 +0,0 @@
-import time
-
-
-def interval_passed(threadName, message, interval: int):
-    while True:
-        time.sleep(interval)
-        print(f"thread: {threadName}, message: {message}, interval: {interval}")
