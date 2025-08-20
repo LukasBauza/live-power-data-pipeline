31475c4ea17fba386678a733ae9f2c79baa8d0c5
Lukas Bauza <lukasbauza.lb@proton.me>
2025-08-20 17:27:08 +0100

    create_timestamp should now return the pandas.Timestamp. If incorrect timestamp then it returns current time, with warning.

--- src/timestamp.py
+++ src/timestamp.py
@@ -1,16 +1,21 @@
 import pandas
 
-from config import TIME_ZONE
-from logger import log
+from .config import TIME_ZONE
+from .logger import log
 
 
-def create_timestamp(date: str, future: bool = False):
+def create_timestamp(date: str, future: bool = False) -> pandas.Timestamp:
+    now = pandas.Timestamp.now(tz=TIME_ZONE)
     try:
-        now = pandas.Timestamp.now(tz=TIME_ZONE)
         timestamp = pandas.Timestamp(date, tz=TIME_ZONE)
 
         if timestamp > now and future == False:
             log.warning(f"Date {timestamp} is in the future.")
 
+        return timestamp
+
     except Exception as e:
-        log.error(f"Error processing timestamp '{date}': {e}")
+        log.error(f"Issue processing timestamp '{date}': {e}")
+        log.warning(f"Using current time '{now}'.")
+
+        return now
