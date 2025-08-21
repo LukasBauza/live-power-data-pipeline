63a2678f36a316eb923623ce7c808a4706d55af2
Lukas Bauza <lukasbauza.lb@proton.me>
2025-08-21 11:06:40 +0100

    Updated save function Made sure that the save function can now append the data to the file instead of always overwriting it, and made sure that the header is only added if the file didn't exist initially.

--- src/save.py
+++ src/save.py
@@ -1,20 +1,29 @@
-from os import makedirs
-from os.path import dirname
+from os import makedirs, stat
+from os.path import dirname, isfile
+import pandas
+
 from .config import RAW_DIR
 from .logger import log
 
 
-def save_csv(data_type: str, country_code: str, data):
+def ensure_dir_exists(dir: str):
+    try:
+        makedirs(dir, exist_ok=True)
+    except Exception as e:
+        log.error(f"Error creating directory: {e}")
+
+
+def save_csv(data_type: str, country_code: str, data: pandas.DataFrame):
     file_path = f"{RAW_DIR}/{data_type}_{country_code}.csv"
 
-    try:
-        # NOTE: dirname() needed?
-        makedirs(dirname(RAW_DIR), exist_ok=True)
-    except Exception as e:
-        log.error(f"Error creating directory")
+    ensure_dir_exists(RAW_DIR)
 
     try:
-        data.to_csv(file_path, mode="a")
+        # Only add the header if it doesn't exist
+        header_exists = isfile(file_path)
+        data.to_csv(file_path, mode="a", header=not header_exists, index=True)
+        # Custom headers created, as for some reason the timestamp header is not created,
+        # but timestamp data is written to file
         log.info(f"Data saved to {file_path}")
     except Exception as e:
         log.error(f"Failed writing to {file_path}: {e}")
