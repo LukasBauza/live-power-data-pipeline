e0f0901e6a302e32de5266637969ed402c5f7e12
Lukas Bauza <LukasBauza.LB@proton.me>
2025-08-21 22:23:20 +0100

    Removed unecessary function This is because the API key wasn't being saved properly.

--- src/config.py
+++ src/config.py
@@ -6,7 +6,3 @@ TIME_ZONE = "Europe/Dublin"
 DATA_DIR = "data"
 RAW_DIR = f"{DATA_DIR}/raw"
 PROCESSED_DIR = f"{DATA_DIR}/processed"
-
-
-def enter_api():
-    API_KEY = getpass.getpass("Please enter your API key: ")
