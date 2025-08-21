41a145715bc3e71517e7ca4af5f06ca1dcfd7e05
Lukas Bauza <LukasBauza.LB@proton.me>
2025-08-21 21:51:45 +0100

    Fixed issues with API key being entered was not saved.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import asyncio
+import getpass
 
 from src.pull import LoadPuller
 from src.config import COUNTRY_CODE, TIME_ZONE, enter_api
 
 
 def main():
-    enter_api()
+    api_key = getpass.getpass("Please enter your API key: ")
 
-    dublin_load_puller = LoadPuller(COUNTRY_CODE, TIME_ZONE)
+    dublin_load_puller = LoadPuller(COUNTRY_CODE, TIME_ZONE, api_key)
     asyncio.run(dublin_load_puller.hourly_pull())
 
 
