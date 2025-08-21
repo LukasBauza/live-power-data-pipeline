396a07c478a0d47bbbdddc5f8ca771c3c98bbb77
Lukas Bauza <LukasBauza.LB@proton.me>
2025-08-21 20:32:14 +0100

    Removed API Key (and no longer usable) Allow the user to enter their API key when running the script.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import asyncio
 
 from src.pull import LoadPuller
-from src.config import COUNTRY_CODE, TIME_ZONE
+from src.config import COUNTRY_CODE, TIME_ZONE, enter_api
 
 
 def main():
+    enter_api()
+
     dublin_load_puller = LoadPuller(COUNTRY_CODE, TIME_ZONE)
     asyncio.run(dublin_load_puller.hourly_pull())
 
--- src/config.py
+++ src/config.py
@@ -1,7 +1,12 @@
-# TODO: Check if this is the best way to use the api key.
-API_KEY = "944ec6ae-94a0-4508-9198-d85bd9d09cdc"
+import getpass
+
+API_KEY = ""
 COUNTRY_CODE = "IE"
 TIME_ZONE = "Europe/Dublin"
 DATA_DIR = "data"
 RAW_DIR = f"{DATA_DIR}/raw"
 PROCESSED_DIR = f"{DATA_DIR}/processed"
+
+
+def enter_api():
+    API_KEY = getpass.getpass("Please enter your API key: ")
